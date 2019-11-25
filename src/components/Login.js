94251ea529526c7533bff9fba3a6d20ff5dc4d6d
kantivekariya <hiren.patel@medsymphony.com>
2019-11-25 18:05:01 +0530

    completed Registration and Login modules

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,27 +4,66 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                email: '',
+                password: ''
+            },
+            submitted: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const { user } = this.state;
+        if (user.email && user.password) {
+            console.log(user)
+        }
+    }
+
     render() {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Login App</h2>
-                <form noValidate autoComplete="off">
+                <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
                     <div>
                         <TextField
                             required
                             label="Email"
                             margin="normal"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
                         />
                         <TextField
                             required
                             label="Password"
                             margin="normal"
+                            name="password"
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
                         />
                     </div>
-                    <Button variant="contained" color="primary">
-                        Login
-                    </Button>
-                    <Link to="/register" className="btn btn-link">Register</Link>
+                    <div className="mt-3">
+                        <Button variant="contained" type="submit" color="primary">Login </Button>
+                        <Link to="/register" className="btn btn-link">Register</Link>
+                    </div>
                 </form>
             </div>
         );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,19 +1,85 @@
 import React from "react";
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                password: ''
+            },
+            submitted: false
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const { user } = this.state;
+        if (user.firstName && user.lastName && user.email && user.password) {
+            console.log(user)
+        }
+    }
+
     render() {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Register App</h2>
-                <form>
-                    <TextField
-                        required
-                        id="standard-required"
-                        label="UserName"
-                        defaultValue="Hello World"
-                        margin="normal"
-                    />
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <div>
+                        <TextField
+                            required
+                            label="First Name"
+                            margin="normal"
+                            name="firstName"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                        <TextField
+                            required
+                            label="Last Name"
+                            margin="normal"
+                            name="lastName"
+                            value={this.state.lastName}
+                            onChange={this.handleChange}
+                        />
+                        <TextField
+                            required
+                            label="Email"
+                            margin="normal"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                        <TextField
+                            required
+                            label="Password"
+                            margin="normal"
+                            name="password"
+                            type="password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="mt-3">
+                        <Button variant="contained" type="submit" color="primary">Register </Button>
+                    </div>
                 </form>
             </div>
         );
