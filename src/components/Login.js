c97df0c51d044a7f356e45beb527afda92b436c9
kantivekariya <hiren.patel@medsymphony.com>
2019-11-25 11:43:21 +0530

    initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="text-left">
+                <h1>Working</h1>
+            </div>
+        );
+    };
+}
+export default Login;
