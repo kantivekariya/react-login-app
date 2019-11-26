62628844ea7b5833c83e71a50a28474d1a1e63a9
kantivekariya <hiren.patel@medsymphony.com>
2019-11-26 18:36:45 +0530

    create home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
+import axios from 'axios';
+
+// function getAll() {
+//     return axios.get('http://localhost:3600/users/');
+// }
 class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.getAll();
+        // const res = getAll();
+    }
+
+    getAll(){
+        axios.get('http://localhost:3600/users/').then((res)=>{
+            console.log(res.data);
+        });
+    }
     render() {
         return (
             <div>
--- src/shared/_services.js
+++ src/shared/_services.js
@@ -1,25 +1,10 @@
 import axios from 'axios';
 
 export const userService = {
-    register,
+    getAll
 };
-// function login(username, password) {
-//     const requestOptions = {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({ username, password })
-//     };
 
-//     return fetch(`${config.apiUrl}/users/authenticate`, requestOptions)
-//         .then(handleResponse)
-//         .then(user => {
-//             // store user details and jwt token in local storage to keep user logged in between page refreshes
-//             localStorage.setItem('user', JSON.stringify(user));
 
-//             return user;
-//         });
-// }
-
-function register(user) {
-    return axios.post('http://localhost:3600/register/', user).then(res => console.log(res.data))
+function getAll() {
+    return axios.post('http://localhost:3600/users');
 }
